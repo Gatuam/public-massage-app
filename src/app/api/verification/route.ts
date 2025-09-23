3fe4469749052323ef6675335254182fca6fb42d
EK GAUTHAM <sggtm42@gmail.com>
2025-09-23 18:01:19 +0700

    verify the code

--- src/app/api/verification/route.ts
+++ src/app/api/verification/route.ts
@@ -1,12 +1,55 @@
 import { dbConnect } from "@/db";
-import { NextRequest } from "next/server";
+import { UserModel } from "@/model/user";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    try {
-        await dbConnect();
-        const {code} = await req.json();
-        
-    } catch (error) {
-        
+  try {
+    await dbConnect();
+    const { code } = await req.json();
+    const user = await UserModel.findOne({
+      verifyCode: code,
+    });
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User dont not exist! please login 1ft",
+        },
+        {
+          status: 404,
+        }
+      );
     }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeExpired = new Date(user.verifyCodeExp.getTime()) < new Date();
+
+    if (isCodeExpired || !isCodeValid) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid verification code",
+        },
+        { status: 403 }
+      );
+    }
+
+    user.isVerfied = true;
+    await user.save();
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Verified successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal error",
+      },
+      { status: 500 }
+    );
+  }
 }
